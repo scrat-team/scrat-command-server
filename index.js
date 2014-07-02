23eb386aae73a8a63fcb92a15eab0cd17b3ef66f
fouber <fouber.NET@gmail.com>
2014-07-02 10:52:50 +0800

    bugfix

--- index.js
+++ index.js
@@ -177,7 +177,7 @@ exports.register = function(commander) {
                     process.stdout.write(' δ '.bold.yellow);
                     var now = Date.now();
                     var include = fis.config.get('server.clean.include', null);
-                    var reg = new RegExp('^' + _.escapeReg(root + '/node_modules/'), 'i');
+                    var reg = new RegExp('^' + fis.util.escapeReg(root + '/node_modules/'), 'i');
                     var exclude = fis.config.get('server.clean.exclude', reg);
                     fis.util.del(root, include, exclude);
                     process.stdout.write((Date.now() - now + 'ms').green.bold);
